0d4b2b669931897cfdc0bc30df3ff1236ddf796d
Kirk Chen <rwk0119@yahoo.com.tw>
2017-08-22 10:33:27 +0800

    Add shopping cart calculator

---
+++ SonarQube.Sample/ShoppingCartCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarQube.Sample
+{
+    public class ShoppingCartCalculator
+    {
+        public int Calculate()
+        {
+            return 0;
+        }
+    }
+}
