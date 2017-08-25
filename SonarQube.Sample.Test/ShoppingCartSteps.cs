0995d4a5fd138f3d4313e3d944c655f924f09c68
Kirk Chen <rwk0119@yahoo.com.tw>
2017-08-25 11:59:51 +0800

    Add first logic for product $500 and VIP member

---
+++ SonarQube.Sample.Test/ShoppingCartSteps.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SonarQube.Sample.Test
+{
+    [Binding]
+    public class ShoppingCartSteps
+    {
+        public string MemberLevel { get; private set; }
+        public int Price { get; private set; }
+        public object TotalPrice { get; private set; }
+
+        [Given(@"會員等級是 ""(.*)""")]
+        public void 假設會員等級是(string memberLevel)
+        {
+            this.MemberLevel = memberLevel;
+        }
+        
+        [Given(@"購物金額為 ""(.*)"" 元")]
+        public void 假設購物金額為元(int price)
+        {
+            this.Price = price;
+        }
+        
+        [When(@"計算總金額")]
+        public void 當計算總金額()
+        {
+            var shoppingCart = new ShoppingCart();
+            this.TotalPrice = shoppingCart.Calculate(this.MemberLevel, this.Price);
+        }
+        
+        [Then(@"總金額是 ""(.*)"" 元")]
+        public void 那麼總金額是元(int totalPrice)
+        {
+            Assert.AreEqual(this.TotalPrice, totalPrice);
+        }
+    }
+}
---
+++ SonarQube.Sample/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SonarQube.Sample
+{
+    public class ShoppingCart
+    {
+
+        public object Calculate(string memberLevel, int price)
+        {
+            return 400;
+        }
+    }
+}
